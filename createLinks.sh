34f4e15899006a66ed5ec103db92032c3f9dbb8a
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-06-29 18:57:44 +0100

    Add mpv and youtube-dl config
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- createLinks.sh
+++ createLinks.sh
@@ -2,3 +2,5 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/youtube-dl ~/.config/youtube-dl
+ln -s ~/dotfiles/mpv.conf ~/.config/mpv/mpv.conf
