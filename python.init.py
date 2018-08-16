f6667ec09af68ecb505a1a96ed2d0633f8442122
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-08-16 14:53:57 +0100

    Added matplotlib to python init
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- python.init.py
+++ python.init.py
@@ -3,6 +3,9 @@ init file for dropdown terminal's python
 '''
 import sys
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 print("Running Python: ", sys.version[:6])
 print("Loaded Numpy: ", np.version.version)
+print("Loaded Matplotlib: ", matplotlib.__version__)
