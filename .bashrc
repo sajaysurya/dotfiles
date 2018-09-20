c50090ae0dd7b87e0db126ac6628b7a4d04aa73b
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-09-20 12:57:13 +0100

    Corrected some fonts and added battery indicator to status
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- .bashrc
+++ .bashrc
@@ -4,3 +4,8 @@ alias ls="ls --color -lah"
 alias rm="rm -i"
 
 . /home/sajay/miniconda3/etc/profile.d/conda.sh
+
+# to start x at login
+if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+  exec startx
+fi
---
+++ bin/battery.py
@@ -0,0 +1,69 @@
+'''
+battery indicator for i3blocks
+'''
+from subprocess import check_output
+
+def color(percent):
+    '''
+    to formal colors
+    '''
+    percent = int(percent)
+    if percent < 20:
+        return "#FB0202"
+    if percent < 50:
+        return "#FFCC00"
+    return "#000000"
+
+# stands for charging
+FA_LIGHTNING = "<span color='yellow'><span font='FontAwesome'>\uf0e7</span></span>"
+
+# stands for plugged in
+FA_PLUG = "<span font='FontAwesome'>\uf1e6</span>"
+
+# stands for using battery
+FA_BATTERY = "<span font='FontAwesome'>\uf240</span>"
+
+# stands for unknown status of battery
+FA_QUESTION = "<span font='FontAwesome'>\uf128</span>"
+
+def main():
+    '''
+    c-style main code
+    '''
+    fulltext = ""
+    output = check_output(['acpi'], universal_newlines=True)
+
+    if not output:
+        # stands for no battery found
+        fulltext = "<span color='red'><span font='FontAwesome'>\uf00d\uf240</span></span>"
+    else:
+        # get separate batteries status
+        batteries = output.split("\n")
+
+        # extract percentage left
+        for battery in batteries:
+            if battery != '':
+                text = "  "
+                percent = battery.split(", ")[1].split("%")[0]
+                state = battery.split(": ")[1].split(", ")[0]
+
+                if state == "Discharging":
+                    text += FA_BATTERY + " "
+                elif state == "Full":
+                    text += FA_PLUG + " "
+                elif state == "Unknown":
+                    text += FA_BATTERY + " "
+                elif state == "Charging":
+                    text += FA_LIGHTNING + " "
+                else:
+                    text += FA_QUESTION
+
+                form = '<span color="{}">{}%</span>'
+                text += form.format(color(percent), percent)
+
+                fulltext += text
+
+    print(fulltext)
+
+if __name__ == "__main__":
+    main()
