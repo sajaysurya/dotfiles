66a4e40d9f7e9c0c99682adb3cd99f967a2c456d
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-09-29 20:41:51 +0100

    Added Euro to currency watch
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- bin/currency.py
+++ bin/currency.py
@@ -11,7 +11,8 @@ def main():
     data = requests.get(url=url).json()
     usd = data['rates']['INR']
     gbp = usd/data['rates']['GBP']
-    fulltext = '<span font="FontAwesome">\uf155:{0:.2f} \uf156</span>  <span font="FontAwesome">\uf154:{1:.2f} \uf156</span>'.format(usd, gbp)
+    eur = usd/data['rates']['EUR']
+    fulltext = '<span font="FontAwesome">\uf155:{0:.2f} \uf156</span>  <span font="FontAwesome">\uf154:{1:.2f} \uf156</span>  <span font="FontAwesome">\uf153:{2:.2f} \uf156</span>'.format(usd, gbp, eur)
     print(fulltext)
 
 if __name__ == "__main__":
