8c46e865baa47c8ee48d94cbd96f95ed84255a92
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-11-17 14:07:30 +0000

    Added yen to currency monitor
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- bin/currency.py
+++ bin/currency.py
@@ -12,9 +12,8 @@ def main():
     usd = data['rates']['INR']
     gbp = usd/data['rates']['GBP']
     eur = usd/data['rates']['EUR']
-    eur = usd/data['rates']['EUR']
     jpy = usd/data['rates']['JPY']
-    fulltext = '<span font="FontAwesome">\uf155:{0:.2f} \uf156</span>  <span font="FontAwesome">\uf154:{1:.2f} \uf156</span>'.format(usd, gbp)
+    fulltext = '<span font="FontAwesome">\uf155:{0:.2f} \uf156</span>  <span font="FontAwesome">\uf154:{1:.2f} \uf156</span>  <span font="FontAwesome">\uf157:{2:.2f} \uf156</span>'.format(usd, gbp, jpy)
     print(fulltext)
 
 if __name__ == "__main__":
