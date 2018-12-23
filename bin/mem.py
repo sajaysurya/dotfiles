dca3e902ba0ed7605e06cf940872df1be90c0baf
Sajaysurya Ganesh <sajay.surya@gmail.com>
2018-12-23 11:55:17 +0530

    Corrected the hdd size display in i3status
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- bin/mem.py
+++ bin/mem.py
@@ -9,7 +9,7 @@ def main():
     '''
     mem = int(check_output('free | grep Mem', shell=True).decode().split(' ')[-1])
     fulltext = "<span font='FontAwesome'>\uf16c</span> {0:.2f}GiB".format(mem/1e6)
-    dsk = check_output('df -h | grep /dev/sda1', shell=True).decode().split()[1]
+    dsk = check_output('df -h | grep ubuntu', shell=True).decode().split()[3]
     fulltext += " <span font='FontAwesome'>\uf0a0</span> {}iB".format(dsk)
     print(fulltext)
 
