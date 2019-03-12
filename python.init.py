1f307d0cb08f749716567d638c1cb45ce2d2be82
Sajaysurya Ganesh <sajay.surya@gmail.com>
2019-03-12 17:50:24 +0000

    Changed deps and python.init
    Signed-off-by: Sajaysurya Ganesh <sajay.surya@gmail.com>

--- python.init.py
+++ python.init.py
@@ -14,6 +14,9 @@ b = np.arange(4)
 x = sp.symbols('x')
 y = sp.symbols('y')
 
+l = [0,1,2,3,4,5,6,7,8,9]
+d = pd.DataFrame(a, columns=['a','b','c','d'])
+
 print("Running Python: ", sys.version[:6])
 print("Loaded Numpy: ", np.version.version, "with a matrix and b vector")
 print("Loaded Sympy: ", sp.__version__, "with x, y symbols")
