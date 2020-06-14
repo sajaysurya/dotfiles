18a26bcd3218417842ed9cdff249bb41e989a3bb
Sajaysurya Ganesh <sajay@macoe.local>
2020-06-14 12:38:20 +0100

    added swapkeys for mac

---
+++ bin/swapkeys.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+hidutil property --set '{
+"UserKeyMapping":[
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000064,
+        "HIDKeyboardModifierMappingDst":0x700000035
+    },
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000035,
+        "HIDKeyboardModifierMappingDst":0x7000000E1
+    },
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000029,
+        "HIDKeyboardModifierMappingDst":0x700000039
+    },
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000039,
+        "HIDKeyboardModifierMappingDst":0x700000029
+    },
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000031,
+        "HIDKeyboardModifierMappingDst":0x700000028
+    },
+    {
+        "HIDKeyboardModifierMappingSrc":0x700000028,
+        "HIDKeyboardModifierMappingDst":0x700000031
+    },
+]}'
